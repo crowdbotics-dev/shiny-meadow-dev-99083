8df0f7fc4c24fa662f1e72daab6d5bc438d64185
Crowdbotics <engineering@crowdbotics.com>
2023-07-08 23:07:31 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shiny-meadow-dev-99083",
+  url: "https://shiny-meadow-dev-99083.botics.co",
+  api: "https://shiny-meadow-dev-99083.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
